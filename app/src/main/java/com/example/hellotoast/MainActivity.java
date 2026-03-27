4755f567ec2492941624bba2eb4d0340b653fd7b
ASSEKNOUR Sana <S.asseknour6738@uca.ac.ma>
2026-03-27 17:34:17 +0100

    HelloToast project

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.hellotoast;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
+    private TextView textCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textCount = findViewById(R.id.text_count);
+        Button buttonToast = findViewById(R.id.button_toast);
+        Button buttonCount = findViewById(R.id.button_count);
+
+        buttonToast.setOnClickListener(v -> {
+            Toast.makeText(this, "Bonjour C'est Sana !", Toast.LENGTH_SHORT).show();
+        });
+
+        buttonCount.setOnClickListener(v -> {
+            count++;
+            textCount.setText(String.valueOf(count));
+        });
+    }
+}
